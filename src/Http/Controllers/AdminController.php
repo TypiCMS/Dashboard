64705966d67d13a25780e81de0a1231eef828461
Samuel De Backer <sdebacker@gmail.com>
2019-09-02 15:04:38 +0200

    return type declarations

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -2,28 +2,32 @@
 
 namespace TypiCMS\Modules\Dashboard\Http\Controllers;
 
+use Exception;
+use GuzzleHttp\Client;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 use TypiCMS\Modules\Core\Http\Controllers\BaseAdminController;
 
 class AdminController extends BaseAdminController
 {
-    /**
-     * Redirect to dashboard.
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function index()
+    public function index(): RedirectResponse
     {
         return redirect(route('dashboard'));
     }
 
-    /**
-     * Admin home.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function dashboard()
+    public function dashboard(Client $client): View
     {
-        return view('dashboard::show')
-            ->with('welcomeMessage', $this->model->welcomeMessage());
+        $welcomeMessage = config('typicms.welcome_message');
+        $url = config('typicms.welcome_message_url');
+        try {
+            $response = $client->get($url);
+            if ($response->getStatusCode() < 400) {
+                $welcomeMessage = $response->getBody();
+            }
+        } catch (Exception $exception) {
+            info($exception->getMessage());
+        }
+
+        return view('dashboard::show')->with(compact('welcomeMessage'));
     }
 }

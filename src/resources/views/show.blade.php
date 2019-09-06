35a6713c493ddddb2de019650d6e6b91b62cbecb
Samuel De Backer <sdebacker@gmail.com>
2019-09-06 14:01:50 +0200

    History: sorting and pagination

--- src/resources/views/show.blade.php
+++ src/resources/views/show.blade.php
@@ -25,7 +25,23 @@
         </div>
 
         @can('see-history')
-        <history @can('clear-history'):clear-button="true"@endcan></history>
+        <history
+            fields="history.id,history.created_at,history.title,history.locale,history.icon_class,history.historable_id,history.historable_type,history.action,history.user_id"
+            include="historable"
+            :searchable="['title,historable_type,action,user_name']"
+            :sorting="['-created_at']"
+            @can('clear-history'):clear-button="true"@endcan
+            >
+
+            <template slot="columns" slot-scope="{ sortArray }">
+                <item-list-column-header name="created_at" sortable :sort-array="sortArray" :label="$t('Date')"></item-list-column-header>
+                <item-list-column-header name="title" sortable :sort-array="sortArray" :label="$t('Title')"></item-list-column-header>
+                <item-list-column-header name="historable_type" sortable :sort-array="sortArray" :label="$t('Module')"></item-list-column-header>
+                <item-list-column-header name="action" sortable :sort-array="sortArray" :label="$t('Action')"></item-list-column-header>
+                <item-list-column-header name="user_name" sortable :sort-array="sortArray" :label="$t('User')"></item-list-column-header>
+            </template>
+
+        </history>
         @endcan
 
     </div>
